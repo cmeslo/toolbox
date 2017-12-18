324c94cebff28bbe86c9398adc54d5da595604dc
Ray <rm-xp@hotmail.com>
2017-12-18 11:25:05 +0800

    Create create_file.py

---
+++ create-file/create_file.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def create(file_name, row_len, col_len):
+
+    with open(file_name, 'wb') as f:
+        for r in range(row_len):
+            row = b''
+            for c in range(col_len):
+                row += b'0'
+            row += b'\r'
+            f.write(row)
+
+    f.close()
+
+if __name__ == "__main__":
+    # create('create_test.txt', 4, 1195)
+    if len(sys.argv) == 4:
+        create(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+        print('create file ' + sys.argv[1] + ' success.')
+    else:
+        print('parameter error!')
