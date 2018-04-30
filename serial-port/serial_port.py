effad13a9efbfe51baab1e9211a7b7976fc1a018
Ray <rm-xp@hotmail.com>
2018-04-30 17:01:42 +0800

    Update serial_port.py

--- serial-port/serial_port.py
+++ serial-port/serial_port.py
@@ -3,11 +3,11 @@ import time
 
 if __name__ == "__main__":
 
-    # card query, 0x32 = 0x02 ^ 0x00 ^ 0x30 ^ 0x00
+    # card query
     command1 = [0x02,0x02,0x00,0x30,0x00,0x32,0x03]
     bArray1 = bytearray(command1)
 
-    # card consume, 0x75 = 0x10 ^ 0x00 ^ 0x40 ^ ... ^ 0x04 ^ 0x46
+    # card consume
     command2 = [0x02,0x10,0x00,0x40,0x00,0x00,0x00,0x00,0x00,0x10,0x10,0x20,
                 0x18,0x10,0x03,0x08,0x17,0x56,0x05,0x04,0x46,0x75,0x03]
     bArray2 = bytearray(command2)
@@ -15,9 +15,12 @@ if __name__ == "__main__":
     ser = serial.Serial("COM1", 9600, timeout=3)
     # ser.open()
     ser.write(bArray1)
-    out = ser.readline()
-    print(''.join('{:02x}'.format(x) for x in out))
-    time.sleep(3)
-    
+    # out = ser.readline()
+    out = ser.readall()
+    print(''.join('{:02x} '.format(x) for x in out))
+    # time.sleep(3)
+
     ser.write(bArray2)
+    out = ser.readall()
+    print(''.join('{:02x} '.format(x) for x in out))
     ser.close()
