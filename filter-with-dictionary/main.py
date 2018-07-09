73de1eff268caf45f0219d7383621b04970c9f43
Ray <rm-xp@hotmail.com>
2018-07-09 18:43:15 +0800

    Update main.py

--- filter-with-dictionary/main.py
+++ filter-with-dictionary/main.py
@@ -9,12 +9,12 @@ def build_dictionary(path):
 
 
 def handle_file(d, path):
-    with open('./new1.txt', 'w') as new_file1, open('./new2.txt', 'w') as new_file2:
+    with open('./result1.txt', 'w') as new_file1, open('./result2.txt', 'w') as new_file2:
         with open(path) as old_file:
             for line in old_file:
-                key = line[96:113]
+                key = line[7:11]
                 if key in d:
-                    new_line = line[0:96] + d[key] + line[113:]
+                    new_line = line[0:7] + d[key] + line[11:]
                     new_file1.write(new_line)
                 else:
                     new_file2.write(line)
@@ -22,7 +22,7 @@ def handle_file(d, path):
 
 def main():
     d = build_dictionary('./dict.txt')
-    handle_file(d, './ABCS_REPORT_FILE_20180601.TXT')
+    handle_file(d, './test.txt')
 
 
 if __name__ == "__main__":
