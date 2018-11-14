ce26d5d3b9477eec3eebdf83f995aaa6fef78aaa
Ray <rm-xp@hotmail.com>
2018-11-14 14:40:46 +0800

    Update main.py

--- compare/main.py
+++ compare/main.py
@@ -1,11 +1,6 @@
 def build_list(path):
-    array = []
     with open(path) as f:
-        for line in f:
-            # (num, mnt) = line.strip().split(',')
-            # array.append((num, mnt))
-            array.append(line.strip())
-    return array
+        return [line.strip() for line in f]
 
 
 def compare(list1, list2):
@@ -14,18 +9,11 @@ def compare(list1, list2):
             if item in list(list1):
                 list1.remove(item)
                 list2.remove(item)
-                # same.write(''.join('{},{}'.format(item[0], item[1])) + '\n')
                 same.write(item + '\n')
 
-    with open('./data/diff1.txt', 'w') as diff1:
-        for item in list1:
-            # diff1.write(''.join('{},{}'.format(item[0], item[1])) + '\n')
-            diff1.write(item + '\n')
-
-    with open('./data/diff2.txt', 'w') as diff2:
-        for item in list2:
-            # diff2.write(''.join('{},{}'.format(item[0], item[1])) + '\n')
-            diff2.write(item + '\n')
+    with open('./data/diff1.txt', 'w') as diff1, open('./data/diff2.txt', 'w') as diff2:
+        diff1.write('\n'.join(item for item in list1))
+        diff2.write('\n'.join(item for item in list2))
 
 
 def main():
